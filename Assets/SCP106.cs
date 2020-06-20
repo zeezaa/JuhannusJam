f9c9b18f29ea5c1d06f9affb5077d7c4f0bbc9b4
zeezaa <ziizaa123@gmail.com>
2020-06-20 17:54:42 +0300

    SCP106

---
+++ Assets/SCP106.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SCP106 : MonoBehaviour
+{
+    public Transform player;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+    }
+}
