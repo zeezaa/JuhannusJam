dec54f19b06096e837f03af9b720289e24fe1ddf
jaketzu <jaakko.nevala007@gmail.com>
2020-06-20 17:53:22 +0300

    SCP-173 +fixes

---
+++ Assets/Scripts/SCP173.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SCP173 : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public GameObject player;
+    private Camera cam;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, Mathf.Infinity))
+        {          
+            if (hit.transform.CompareTag("Wall"))
+            {
+                agent.isStopped = false;
+                agent.SetDestination(player.transform.position);
+                Debug.DrawRay(transform.position, player.transform.position * hit.distance, Color.blue);
+            }
+
+            if (hit.transform.CompareTag("Player"))
+            {
+                agent.isStopped = true;
+                Debug.Log("Player");
+                Debug.DrawRay(transform.position, player.transform.position * hit.distance, Color.red);
+            }
+        }
+
+        if (cam.enabled == false)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(player.transform.position);
+        }
+    }
+}
